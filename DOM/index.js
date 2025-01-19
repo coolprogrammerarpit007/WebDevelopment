3826187994d58be1836a9e00fc73c6ee8ae11eaf
arpit mishra <arpit.mishra.out@gmail.com>
2025-01-19 11:50:41 +0530

    DOM IN JS

---
+++ DOM/index.js
@@ -0,0 +1,11 @@
+`use strict`;
+console.log(`hello world!`);
+
+// Built In Variables and Functions
+// 1. Global Window Object:- Holds Information and functuanillity related to the active window/tab
+
+// console.log(window);
+// 2. document Object:- holds Info and functuanillity regarding loaded website content
+
+// console.dir(document.body.ch);
+document.body.children[1].children[0].href = "https://www.google.com";
